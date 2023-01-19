e93df756cffce7721f1117319f8b337ed2d4de99
01-ADEK-10 <47279655+01-ADEK-10@users.noreply.github.com>
2023-01-19 10:27:27 +0700

    Update rgb.js

--- rgb.js
+++ rgb.js
@@ -1,13 +1,16 @@
 const buttonW = document.getElementById("white");
 const buttonB = document.getElementById("blue");
 const buttonR = document.getElementById("red");
+const buttonSt = document.getElementById("strob");
 const colors = ["white", "blue", "red"]
+const colors2 = ["rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 1)"]
 // console.log(buttonW);
+let count = 0;
+let alpha = 0;
+var timer;
 
 buttonW.addEventListener("click", () => {
-// console.log("click");
 document.body.style.backgroundColor = colors[0];
-
 });
 
 
@@ -21,5 +24,30 @@ document.body.style.backgroundColor = colors[1];
 buttonR.addEventListener("click", () => {
 // console.log("click");
 document.body.style.backgroundColor = colors[2];
+
+});
+
+buttonSt.addEventListener("click", () => {
+ count++;
+ 
+	if (count < 2) {
+  
+	strob();
+	function strob() {
+			alpha++;
+			document.querySelector(".parent").style.background = colors2[alpha];
+			alpha = -alpha;
+			console.log("click");
+			timer = setTimeout(strob, 500);
+  }
+   
+  } else {
+    clearTimeout(timer); 
+	document.querySelector(".parent").style.background = "rgba(0, 0, 0, 0)";
+	count = 0;
+  
+}
+ 
+ 
 
 });
